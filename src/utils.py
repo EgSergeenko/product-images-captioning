492669c1b2ff3f1469f99e35a1f8e8267db69a61
Egor Sergeenko <es.egor.sergeenko@gmail.com>
2022-11-10 23:26:31 +0300

    Add new functions to utils.py

--- src/utils.py
+++ src/utils.py
@@ -4,27 +4,54 @@ import torch
 def get_max_length(texts, tokenizer):
     max_length = 0
     for text in texts:
+        text = add_special_tokens(
+            text,
+            tokenizer.bos_token,
+            tokenizer.eos_token,
+        )
         labels = tokenizer(
             text,
             return_tensors='pt',
-        ).input_ids.squeeze()
-        max_length = max(max_length, labels.size()[0])
+        ).input_ids
+        max_length = max(max_length, labels.size()[-1])
     return max_length
 
 
-def get_prediction(
+def get_predictions(
     model,
     tokenizer,
-    pixel_values,
-    max_length=64,
-    num_beams=4,
+    data_loader,
+    device,
 ):
-    with torch.no_grad():
-        output_ids = model.generate(
-            pixel_values.unsqueeze(0),
-            max_length=max_length,
-            num_beams=num_beams,
-            return_dict_in_generate=True,
-        ).sequences.detach().cpu()
-    prediction = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
-    return [token.strip() for token in prediction]
+    predictions = []
+    model.eval()
+    for idx, batch in enumerate(data_loader):
+        pixel_values = batch['pixel_values'].to(device)
+        labels = batch['labels'].to(device)
+
+        with torch.no_grad():
+            output_ids = model.generate(
+                pixel_values,
+                max_length=labels.size()[-1],
+                return_dict_in_generate=True,
+            ).sequences
+
+        prediction = tokenizer.batch_decode(
+            output_ids,
+            skip_special_tokens=True,
+        )
+        predictions.extend(prediction)
+
+    return predictions
+
+
+def add_special_tokens(
+    text: str,
+    bos_token: str,
+    eos_token: str,
+) -> str:
+    return '{0} {1} {2}'.format(
+        bos_token,
+        text,
+        eos_token,
+    )
